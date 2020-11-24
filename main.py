e657f9f075e9c345c81ebc8b548dcce77956ebb8
Christopher <christopher.barberon@orange.fr>
2020-11-24 16:23:18 +0100

    avancement noir et blanc

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import os
-
+import cv2
 
 def img_filter (redirectory):
     if not os.path.exists('C:/Users/admin/Documents/repository git hub/image_filter/python-image-filter-groupe-3-1/output'):
         os.mkdir('C:/Users/admin/Documents/repository git hub/image_filter/python-image-filter-groupe-3-1/output')
 
     input = open(redirectory, 'r')
-    output =open('output.txt','w')
+    output =open('output/image1.jpg','w')
 
+    image = cv2.imread('imgs/image1.jpg')
+    grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    output.write(input.read())
+    cv2.imshow('image grise',grey)
+    cv2.imwrite('output',grey,None)
 
     input.close()
     output.close()
 
-os.mkdir('C:/Users/admin/Documents/repository git hub/image_filter/python-image-filter-groupe-3-1/output')
-img_filter('C:/Users/admin/Documents/repository git hub/image_filter/python-image-filter-groupe-3-1/input.txt')
+img_filter('C:/Users/admin/Documents/repository git hub/image_filter/python-image-filter-groupe-3-1/imgs/image1.jpg')
+
