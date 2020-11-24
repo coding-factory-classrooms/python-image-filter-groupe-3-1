eca209000e67cdb3ec0c791a51ed12e781579fdd
TIRNA <contact@tirna.eu>
2020-11-24 16:36:46 +0100

    Version avec la conversion en gris

--- main.py
+++ main.py
@@ -2,20 +2,15 @@ import os
 import cv2
 
 def img_filter (redirectory):
-    if not os.path.exists('C:/Users/admin/Documents/repository git hub/image_filter/python-image-filter-groupe-3-1/output'):
-        os.mkdir('C:/Users/admin/Documents/repository git hub/image_filter/python-image-filter-groupe-3-1/output')
+    if not os.path.exists('/Users/sco/Desktop/TRAVAIL/imageFilter/output'):
+        os.mkdir('/Users/sco/Desktop/TRAVAIL/imageFilter/output')
 
-    input = open(redirectory, 'r')
-    output =open('output/image1.jpg','w')
+    lastImage = cv2.imread('imgs/image1.jpg')
+    newImage = cv2.cvtColor(lastImage, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite('imgs/imageGrey.jpg', newImage)
 
-    image = cv2.imread('imgs/image1.jpg')
-    grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('image grise',grey)
-    cv2.imwrite('output',grey,None)
 
-    input.close()
-    output.close()
 
-img_filter('C:/Users/admin/Documents/repository git hub/image_filter/python-image-filter-groupe-3-1/imgs/image1.jpg')
 
+img_filter('/Users/sco/Desktop/TRAVAIL/imageFilter/imgs/image1.jpg')
