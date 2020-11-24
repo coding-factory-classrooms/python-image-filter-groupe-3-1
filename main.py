85eb23e423baaa2a99c3737f8b807e1db6f44cf6
TIRNA <contact@tirna.eu>
2020-11-24 17:26:35 +0100

    Done story 2,3,4

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import os
 import cv2
-
-def img_filter (redirectory):
-    if not os.path.exists('/Users/sco/Desktop/TRAVAIL/imageFilter/output'):
-        os.mkdir('/Users/sco/Desktop/TRAVAIL/imageFilter/output')
-
-    lastImage = cv2.imread('imgs/image1.jpg')
-    newImage = cv2.cvtColor(lastImage, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite('imgs/imageGrey.jpg', newImage)
+import numpy as np
 
 
+def imageTransform(redirectory, filter):
+    if not os.path.exists('output'):
+        os.mkdir('output')
+
+    lastImage = cv2.imread(redirectory)
+
+    if filter == 'grey':
+        newImage = cv2.cvtColor(lastImage, cv2.COLOR_BGR2GRAY)
+        imageFilter = "greyImage"
+    elif filter == 'blur':
+        sizeImage = (10, 10)
+        newImage = cv2.blur(lastImage, sizeImage)
+        imageFilter = "blurImage"
+    elif filter == 'dilate':
+        kernel = np.ones((5, 5), np.uint8)
+        newImage = cv2.dilate(lastImage, kernel, iterations=3)
+        imageFilter = "dilateImage"
+    else:
+        print("Vous n'avez pas choisis un filtre disponible")
+
+    cv2.imwrite(f'output/{imageFilter}.jpg', newImage)
 
 
 
-img_filter('/Users/sco/Desktop/TRAVAIL/imageFilter/imgs/image1.jpg')
+
+imageTransform('imgs/image1.jpg', 'dilate')
