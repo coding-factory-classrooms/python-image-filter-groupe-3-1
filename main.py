81548b64313c238bf9bbdd4ecda9c7e40482701c
Christopher <christopher.barberon@orange.fr>
2020-11-25 16:16:31 +0100

    merge commit

--- main.py
+++ main.py
@@ -5,56 +5,57 @@ from function.log_function import dump_log
 from function.createimg_function import createImgFunction
 createImgFunction()
 
-#displayAllFilter()
-#dump_log()
 
-
-            print('test')
-        else:
-
-
-                selectionFunction(redirectory, output, finalFilter, finalParamFilter)
-                    finalParamFilter = 0
-                    finalFilter = "grey"
-                else:
-                    finalParamFilter = paramFilter[1]
-                    finalFilter = paramFilter[0]
-                    paramFilter = filter.split(':')
-                if not filter == "grey":
-                filter3 = cmdExcute[sleepLoop + 3]
-            if sleepLoop + 3 < len(cmdExcute):
-
-            selectionFunction(redirectory, output, finalFilter, finalParamFilter)
-                finalParamFilter = 0
-                finalFilter = "grey"
-            else:
-                finalParamFilter = paramFilter[1]
-                finalFilter = paramFilter[0]
-                paramFilter = filter.split(':')
-            if not filter == "grey":
-        if sleepLoop + 2 < len(cmdExcute):
-            filter2 = cmdExcute[sleepLoop + 2]
-        selectionFunction(redirectory, output, finalFilter, finalParamFilter)
-
-
-                finalParamFilter = 0
-                finalFilter = "grey"
-            else:
-                finalParamFilter = paramFilter[1]
-                finalFilter = paramFilter[0]
-                paramFilter = filter.split(':')
-            filter = cmdExcute[sleepLoop + 1]
-            if not filter == "grey":
-    elif inputCLI == "-f":
-        if sleepLoop + 1 < len(cmdExcute):
-            print('test')
-        else:
-            output = cmdExcute[sleepLoop + 1]
-        if sleepLoop + 1 < len(cmdExcute):
-    elif inputCLI == "-o":
-        redirectory = cmdExcute[sleepLoop + 1]
-
-    if inputCLI == "-i":
+cmdExcute = sys.argv
 for sleepLoop in range(0, len(cmdExcute)):
     inputCLI = cmdExcute[sleepLoop]
-cmdExcute = sys.argv
+
+    if inputCLI == "-i":
+        redirectory = cmdExcute[sleepLoop + 1]
+    elif inputCLI == "-o":
+        if sleepLoop + 1 < len(cmdExcute):
+            output = cmdExcute[sleepLoop + 1]
+        else:
+            print('test')
+    elif inputCLI == "-f":
+        if sleepLoop + 1 < len(cmdExcute):
+            filter = cmdExcute[sleepLoop + 1]
+            if not filter == "grey":
+                paramFilter = filter.split(':')
+                finalFilter = paramFilter[0]
+                finalParamFilter = paramFilter[1]
+            else:
+                finalFilter = "grey"
+                finalParamFilter = 0
+
+        selectionFunction(redirectory, output, finalFilter, finalParamFilter)
+
+        if sleepLoop + 2 < len(cmdExcute):
+            filter2 = cmdExcute[sleepLoop + 2]
+            if not filter == "grey":
+                paramFilter = filter.split(':')
+                finalFilter = paramFilter[0]
+                finalParamFilter = paramFilter[1]
+            else:
+                finalFilter = "grey"
+                finalParamFilter = 0
+            selectionFunction(redirectory, output, finalFilter, finalParamFilter)
+
+            if sleepLoop + 3 < len(cmdExcute):
+                filter3 = cmdExcute[sleepLoop + 3]
+                if not filter == "grey":
+                    paramFilter = filter.split(':')
+                    finalFilter = paramFilter[0]
+                    finalParamFilter = paramFilter[1]
+                else:
+                    finalFilter = "grey"
+                    finalParamFilter = 0
+                selectionFunction(redirectory, output, finalFilter, finalParamFilter)
+
+
+        else:
+            print('test')
+
+
+#dump_log()
+#displayAllFilter()
