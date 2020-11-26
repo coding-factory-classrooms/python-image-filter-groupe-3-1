913eae2ccae06b07cf19fa9ab4a61fd21e15d59f
TIRNA <contact@tirna.eu>
2020-11-26 12:04:53 +0100

    Fix du système de config.init avec l'association d'un split de le tableau de sortie

--- function/readconfig_function.py
+++ function/readconfig_function.py
@@ -1,5 +1,15 @@
 def readConfigFunction():
+    resultatVar = []
+
     finalVar = open("config.ini","r")
     finalVar = finalVar.read()
-    finalVar = finalVar.endswith("=")
+    finalVar = finalVar.split("\n")
+
     print(finalVar)
+
+    for sleepLoop in range(0,1):
+        finalVar = finalVar[sleepLoop].split("=")
+        finalVar = finalVar[1]
+        resultatVar.insert(0, finalVar)
+
+    print(resultatVar)
